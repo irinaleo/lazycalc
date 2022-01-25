eede6639d466763223ad4b701c02ad346bdf8789
Irina Bejan <irina.bejan@wsu.edu>
2022-01-24 21:21:53 -0800

    Initial Commit

---
+++ LazyCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyCalc
+//
+//  Created by Irina Bejan on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyCalc/LazyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCalcApp.swift
+//  LazyCalc
+//
+//  Created by Irina Bejan on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct LazyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
