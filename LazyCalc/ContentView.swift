794f8778955ef8c1419d9ff0b605d229be6d0012
Irina Bejan <irina.bejan@wsu.edu>
2022-01-24 21:39:21 -0800

    Set up project with initial total and button spaces

--- LazyCalc/ContentView.swift
+++ LazyCalc/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+enum myButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "÷"
+    case multiply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+}
+
 struct ContentView: View {
+    
+    let buttons: [[myButton]] = [
+        [.seven, .eight, .nine]
+    ]
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            // total
+            HStack {
+                Spacer()
+                Text("0")
+                    .bold()
+                    .font(.system(size: 60))
+                    .foregroundColor(.black)
+            }
             .padding()
+            
+            
+            // buttons
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { item in
+                        Button(action: {
+                            
+                        }, label: {
+                            Text(item.rawValue)
+                                .font(.system(size: 35))
+                                .frame(width: 70, height: 70)
+                                .background(Color.teal)
+                                .foregroundColor(.white)
+                                .cornerRadius(35)
+                        })
+                    }
+                }
+            }
+        }
     }
 }
 
